1aaf5ad5bd39ca6dfcd68c97ed3a51c8afb06b09
Joel Plowright <joelplow@gmail.com>
2018-12-09 18:12:00 +0000

    add progress bar

--- src/Decode2.js
+++ src/Decode2.js
@@ -1,8 +1,9 @@
 const ffmpeg = require("fluent-ffmpeg")
 const wav = require("wav")
 const {Transform} = require("stream")
+const ProgressBar = require("progress")
 
-function decode(file) {
+function decode(file, progressReports) {
   var ffmpegCommand = ffmpeg(file)
     .format("wav")
     .audioCodec("pcm_f32le")
@@ -20,6 +21,16 @@ function decode(file) {
     console.log("format:", format)
     FORMAT = format
   })
+
+  if(progressReports) {
+    ffmpegCommand.on("progress", (prog) => {
+      if(!this.bar) {
+        this.bar = new ProgressBar(file+" :percent \t[:bar] ETA: :eta seconds ", {total: 100, width:50})
+      }
+      this.bar.tick(prog.percent-(this.bar.curr || 0))
+    })
+  }
+
 
   var objectify = new Transform({
     //writableObjectMode: true,
